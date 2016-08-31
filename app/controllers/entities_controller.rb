f8d6769919c2bbb9b349bdafc8689694fc80e834
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-31 14:35:51 -0400

    Built entity show api, restricted routes to only api functionality

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -3,4 +3,9 @@ class EntitiesController < ApplicationController
     @entities = Entity.all
     render json: @entities
   end
+
+  def show
+    @entity = Entity.find(params[:id])
+    render json: @entity
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'application#home'
-  resources :entities
+  resources :entities, only: [:show, :index, :create, :update, :destroy]
 end
