fe1e670e6140440a1b45840eb44e7e52ce496d2c
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-09-03 19:44:38 -0400

    Fixed section post redirect

--- app/assets/javascripts/controllers/SectionCtrl.js
+++ app/assets/javascripts/controllers/SectionCtrl.js
@@ -10,9 +10,9 @@ function SectionCtrl (section, SectionService, $state) {
     }
   }
 
-  this.postSection = function () {
-    SectionService.postSection(self.data).then(function (resp) {
-      $state.go('entities.show',{id: resp.data.entity_id})
+  this.postSection = function () { 
+    SectionService.postSection(self.data).then(function (resp) { 
+      $state.go('entities.show',{id: self.data.section.entity_id})
     });
     
   }
