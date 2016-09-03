8e22a2bfd62cf7261e2e4f94cdc1536695393e72
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-09-03 19:09:11 -0400

    Built backend create method for sections, whitelisted parameters

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -8,4 +8,18 @@ class SectionsController < ApplicationController
     section = Section.find(params[:id])
     render json: section
   end
+
+  def create
+    section = Section.new(section_params)
+    if section.save
+      render json: section, status: 201
+    else
+      render json: {status: "Failed to create section"}
+    end
+  end
+
+  private
+    def section_params
+      params.require(:section).permit(:name, :description, :entity_id)
+    end
 end
