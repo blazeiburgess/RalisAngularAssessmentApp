6169ff3a1b98dd60a9ef26dbc2f636c1594f3ac2
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-09-04 15:06:40 -0400

    Built prototype model for subsection

---
+++ app/assets/javascripts/models/Subsection.js
@@ -0,0 +1,7 @@
+var Subsection =  function(subsectionJSON) {
+  this.id = subsectionJSON.id;
+  this.name = subsectionJSON.name;
+  this.description = subsectionJSON.description;
+  this.entity_id = subsectionJSON.entity_id;
+  this.section_id = subsectionJSON.section_id;
+}
