d6c9fb1187093fef0d20892867cd1fc852ccb192
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-09-05 11:00:22 -0400

    Built create method for backend cat ctrl, added unique name validation to model

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,4 +8,18 @@ class CategoriesController < ApplicationController
     category = Category.find(params[:id])
     render json: category
   end
+
+  def create
+    category = Category.new(category_params)
+    if category.save
+      render json: category, status: 201
+    else
+      render json: {status: "Error creating category"}
+    end
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,14 @@
 class Category < ApplicationRecord
   has_many :entity_categories
   has_many :entities, through: :entity_categories
+  before_save :downcase_name
+  validates :name, uniqueness: { case_sensitie: false }
 
   def cats
     entities
+  end
+
+  def downcase_name
+    self.name = self.name.downcase
   end
 end
