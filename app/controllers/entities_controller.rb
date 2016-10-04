eba57d530c03a9df1d299159a0439638205884b2
blazeiburgess <blaze.i.burgess@gmail.com>
2016-10-04 19:17:01 -0400

    Readded uncategorized entities to index view

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,7 +1,7 @@
 class EntitiesController < ApplicationController
   def index
     # @entities = Entity.last(50).sort_by {|entity| entity.name.downcase} + Entity.all.select {|e| e.categories.count == 0}
-    @entities = Entity.last(2000).sort_by {|e| e.name.downcase}
+    @entities = (Entity.last(2000) + Entity.select {|ent| ent.categories.count == 0}).uniq.sort_by {|e| e.name.downcase}
     render json: @entities, each_serializer: SimpleEntitySerializer
   end
 
